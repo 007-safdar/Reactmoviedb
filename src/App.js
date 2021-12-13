d5a49078f15980376acd127cf656de7573a9c0da
xx172 <zeinsafdar@gmail.com>
2021-12-13 22:57:27 +0530

    dev

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line
+import React from 'react';
+import { GlobalStyle } from './GlobalStyles';
+//Components
+
+import Header from './components/Header';
+import Home from './components/Home';
+
+
+//instead of jsx can use react.create elem
+const App = () => {
+    return (
+        <div className="App">
+            <Header/>
+            <Home/>
+            start Here
+            <GlobalStyle/>
+
+            
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line
+import React,{useEffect, useState} from 'react';
+
+//Config
+import {POSTER_SIZE,BACKDROP_SIZE,IMAGE_BASE_URL, API_KEY} from '../config';
+//API
+// import API from '../API';
+import NoImage from '../images/no_image.jpg';
+//hooks
+import { useHomeFetch } from '../hooks/useHomeFetch';
+import HeroImage from '../heroimage';
+const Home=()=>{
+    const {state,loading,error}=useHomeFetch();
+    console.log(state);
+    return(
+        <>
+        {state.results[0]&&
+        <HeroImage image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+        title={state.results[0].original_title}
+        text={state.results[0].overview}/>
+        }   
+        </>
+    )
+}
+export default Home;
